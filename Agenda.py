bf94c273a474703ab85ab755b0bcaf56cab959eb
paolithiago <54008103+paolithiago@users.noreply.github.com>
2020-05-25 21:58:51 -0300

    Create Agenda.py

---
+++ Agenda.py
@@ -0,0 +1,20 @@
+#Agenda Oi paoli
+#1 - Segunda
+#2 - Terca
+#3 - Quarta
+#4 - Quinta
+#5 - Sexta
+dia = int(input("Digite o dia da semana a analisar na Agenda:"))
+if (dia == 1):
+    print("Segunda - Reuniao PSR")
+elif (dia == 2):
+    print("Terça - Reuniao Spoke On line")
+elif (dia == 3):
+    print("Quarta - Preparação Material Reuniao de Resultado")
+elif (dia == 4):
+    print("Quinta - Reuniao Equipe HDM")
+elif (dia == 5):
+    print("Sexta - Reuniao Spoke Off line")
+    
+else:
+    print("Fim de semana")
