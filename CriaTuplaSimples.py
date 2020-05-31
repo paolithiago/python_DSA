69c6ee588882b297e6b6d7131ff1709afebec304
paolithiago <54008103+paolithiago@users.noreply.github.com>
2020-05-31 10:26:40 -0300

    Create CriaTuplaSimples.py

---
+++ CriaTuplaSimples.py
@@ -0,0 +1,5 @@
+#Primeito loop externo . Segundo loop interno.  Para cada item do conjunt de item executa outra operação
+for i in range(0,5):
+    print("mat %r \n"%i)
+    for a in range(0,5):
+        print(a)
